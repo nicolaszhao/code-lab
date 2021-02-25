a12bd51e2713552f6721bfd67374723aa43f34f7
ZhaoXiaodong <zhaoxd@wifi.com>
2021-02-25 11:07:33 +0800

    chore: 加入一个混乱的 nuxt.js 的框架 demo

---
+++ demos/nuxt-app/pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <NuxtLink to="/">home</NuxtLink>
+    <NuxtLink to="/posts">posts</NuxtLink>
+    <NuxtLink to="/about">about</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
