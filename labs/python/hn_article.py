1f5f31db21cf3014df40131479a11555b36d0382
赵晓冬 <nicolas.zhao@wework.cn>
2024-06-09 15:26:51 +0800

    chore: some demo files

---
+++ labs/python/hn_article.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+
+url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+response_dict = r.json()
+readable_file = 'readable_hn_data.json'
+with open(readable_file, 'w') as f:
+  json.dump(response_dict, f, indent=4)
